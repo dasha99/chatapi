cf3a645ab6b2c2b902a215a8732d07bec11ade25
Abhisek Dash <abhishek.dash@gmail.com>
2016-07-06 18:16:34 -0700

    update settings

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from eve import Eve
+import os
 app = Eve()
+if 'PORT' in os.environ:
+    port = int(os.environ.get('PORT'))
+    # use '0.0.0.0' to ensure your REST API is reachable from all your
+    # network (and not only your computer).
+    host = '0.0.0.0'
+else:
+    port = 5000
+    host = '127.0.0.1'
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    app.run(host=host, port=port)
