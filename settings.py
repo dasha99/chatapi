e02784278208b43dd64262738e8496dda65e70cc
Abhisek Dash <abhishek.dash@gmail.com>
2016-07-07 11:40:44 -0700

    Updated user name as searchable

--- settings.py
+++ settings.py
@@ -71,10 +71,10 @@ people = {
     # by default the standard item entry point is defined as
     # '/people/<ObjectId>'. We leave it untouched, and we also enable an
     # additional read-only entry point. This way consumers can also perform
-    # GET requests at '/people/<lastname>'.
+    # GET requests at '/people/<username>'.
     'additional_lookup': {
         'url': 'regex("[\w]+")',
-        'field': 'lastname'
+        'field': 'username'
     },
 
     # We choose to override global cache-control directives for this resource.
