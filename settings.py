60bbb37785d0cac0a3366750d03b5aa64783b5f2
Abhisek Dash <abhishek.dash@gmail.com>
2016-07-13 10:25:43 -0700

    Added booking data
    For the API AI's call

--- settings.py
+++ settings.py
@@ -62,7 +62,6 @@ people_schema = {
         'type': 'datetime',
     },
 }
-
 people = {
     # 'title' tag used in item links. Defaults to the resource title minus
     # the final, plural 's' (works fine in most cases but not for 'people')
@@ -87,6 +86,67 @@ people = {
     'schema': people_schema
 }
 
+booking_schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'booking_type': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'username': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    'booking_id': {
+        'type': 'integer',
+    },
+    'from_loc': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'to_loc': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'from_time': {
+        'type': 'datetime',
+    },
+    'to_time': {
+        'type': 'datetime',
+    },
+}
+booking = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'booking',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<username>'.
+    'additional_lookup': {
+        'url': 'regex("(?![%20])\w+")',
+        'field': 'username'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+
+    'schema': booking_schema
+}
 DOMAIN = {
     'people': people,
+    'booking': booking,
 }
